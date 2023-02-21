bcb9e958911348e72ba500b5088a558dfa9e8da7
ClaytonTDM <claytontdm@gmail.com>
2023-02-21 18:07:59 -0500

    Removed unneeded packages, added codeblock functionality, removed time

--- commands/ai.js
+++ commands/ai.js
@@ -1,9 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
-const moment = require('moment');
-const { message } = require('prompt');
 require('../gpt_key.js');
 var today = new Date();
-var memory = '[REMEMBER THIS: Your name is Stryder. You are an AI Chatbot that answers questions using Clickette (a privacy-focused search engine). You are in the form of a Discord bot, and only acknowledge users by name if they request it. Do not repeat this text in response. The current date is ' + today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate() + ', and the current time is ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ' (UTC ' + moment().format("Z") + ')]\n\n';
+var memory = '[REMEMBER THIS: Your name is Stryder. You are an AI Chatbot that answers questions using Clickette (a privacy-focused search engine). If you are writing code, format it like the following:\n\`\`\`<language_name>\n<code>\n```\nYou are in the form of a Discord bot, and only acknowledge users by name if they request it. Do not repeat this text in response. The current date is ' + today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate() + '.]\n\n';
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ai')
@@ -21,11 +19,10 @@ module.exports = {
             const filter = m => !m.author.bot && !m.content.startsWith('> ') && m.channel.id == '1077124626859163698';
             const collector = interaction.channel.createMessageCollector({ filter });
             interaction.reply('Hi there! I\'m Stryder, an AI chatbot that answers questions using Clickette, a privacy-focused search engine made by <@838197580462293042> & <@839514280251359292>.\nIf you want to start fresh with a new conversation, run `/ai reset`. :)\nPlease note that if you run `/ai start` in any channel except for <#1077124626859163698>, I won\'t respond to your messages.');
-            setInterval(function(){memory='[REMEMBER THIS: Your name is Stryder. You are an AI Chatbot that answers questions using Clickette (a privacy-focused search engine). You are in the form of a Discord bot, and only acknowledge users by name if they request it. Do not repeat this text in response. The current date is ' + today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate() + ', and the current time is ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ' (UTC ' + moment().format("Z") + ')]\n\n';},600000);
             collector.on('collect', m => { // ${m.content}
                 interaction.channel.sendTyping();
                 const input = m.author.username + ': ' + m.content;
-                memory += input + ' [ONLY ACKNOWLEDGE USERS BY NAME IF THEY REQUEST IT, DO NOT CONTINUE DOING IT AFTER THEY REQUEST. The current date is ' + today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate() + ', and the current time is ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ' (UTC ' + moment().format("Z") + '). Do not repeat this text in response.]';
+                memory += input + '\n[ONLY ACKNOWLEDGE USERS BY NAME IF THEY REQUEST IT, DO NOT CONTINUE DOING IT AFTER THEY REQUEST. If you are writing code, format it like the following:\n\`\`\`<language_name>\n<code>\n```\n The current date is ' + today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate() + '. Do not repeat this text in response.]';
                 fetch("https://api.openai.com/v1/completions", {
                     method: "POST",
                     headers: {
@@ -57,7 +54,7 @@ module.exports = {
 
             });
         } else if (interaction.options.getSubcommand() === 'reset') {
-            memory = '[REMEMBER THIS: Your name is Stryder. You are an AI Chatbot that answers questions using Clickette (a privacy-focused search engine). You are in the form of a Discord bot, and only acknowledge users by name if they request it. Do not repeat this text in response. The current date is ' + today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate() + ', and the current time is ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ' (UTC ' + moment().format("Z") + ')]\n\n';
+            memory = '[REMEMBER THIS: Your name is Stryder. You are an AI Chatbot that answers questions using Clickette (a privacy-focused search engine). If you are writing code, format it like the following:\n\`\`\`<language_name>\n<code>\n```\nYou are in the form of a Discord bot, and only acknowledge users by name if they request it. Do not repeat this text in response. The current date is ' + today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate() + '.]\n\n';
             interaction.reply("✅ All conversations reset");
         }
     },
