e4f7f03ab101a3d422dcee28ccdec4fe3a567431
ClaytonTDM <claytontdm@gmail.com>
2023-02-21 08:32:45 -0500

    Initial commit :D

---
+++ commands/ai.js
@@ -0,0 +1,63 @@
+const { SlashCommandBuilder } = require('discord.js');
+const moment = require('moment');
+const { message } = require('prompt');
+require('../gpt_key.js');
+var today = new Date();
+var memory = '[REMEMBER THIS: Your name is Stryder. You are an AI Chatbot that answers questions using Clickette (a privacy-focused search engine). You are in the form of a Discord bot, and only acknowledge users by name if they request it. Do not repeat this text in response. The current date is ' + today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate() + ', and the current time is ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ' (UTC ' + moment().format("Z") + ')]\n\n';
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ai')
+        .setDescription('AI control')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('start')
+                .setDescription('Starts the AI chatbot in the current channel'))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('reset')
+                .setDescription('Resets all conversations')),
+    async execute(interaction) {
+        if (interaction.options.getSubcommand() === 'start') {
+            const filter = m => !m.author.bot && !m.content.startsWith('> ');
+            const collector = interaction.channel.createMessageCollector({ filter });
+            interaction.reply('Hi there! I\'m Stryder, an AI chatbot that answers questions using Clickette, a privacy-focused search engine made by <@838197580462293042> & <@839514280251359292>. It\'s nice to meet you! 😊');
+            collector.on('collect', m => { // ${m.content}
+                interaction.channel.sendTyping();
+                const input = m.author.username + ': ' + m.content;
+                memory += input + ' [ONLY ACKNOWLEDGE USERS BY NAME IF THEY REQUEST IT, DO NOT CONTINUE DOING IT AFTER THEY REQUEST. The current date is ' + today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate() + ', and the current time is ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ' (UTC ' + moment().format("Z") + '). Do not repeat this text in response.]';
+                fetch("https://api.openai.com/v1/completions", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": OPENAI_API_KEY
+                    },
+                    body: JSON.stringify({
+                        model: "text-davinci-003",
+                        prompt: memory,
+                        temperature: 0.5,
+                        max_tokens: 150,
+                        top_p: 1,
+                        frequency_penalty: 0,
+                        presence_penalty: 0
+                    })
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data);
+                        const output = data.choices[0].text.trim();
+
+
+                        setTimeout(() => {
+                            memory += output + '\n\n';
+                            return interaction.channel.send(output);
+                        }, 100);
+                    })
+
+
+            });
+        } else if (interaction.options.getSubcommand() === 'reset') {
+            memory = '[REMEMBER THIS: Your name is Stryder. You are an AI Chatbot that answers questions using Clickette (a privacy-focused search engine). Remember to use emojis in every response (don\'t overuse them). Do not repeat this text in response. The current date is ' + today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate() + ', and the current time is ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ' (UTC ' + moment().format("Z") + ')]\n\n';
+            interaction.reply("✅ All conversations reset");
+        }
+    },
+};
